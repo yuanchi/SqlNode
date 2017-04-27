750a91c52487ba00cae22e2852d7e3e96ae9f991
jerry <eeit54@gmail.com>
2017-04-27 15:40:44 +0800

    SimpleCondition add protocol Expressible

--- Sources/SimpleCondition.swift
+++ Sources/SimpleCondition.swift
@@ -1,6 +1,8 @@
-open class SimpleCondition: SqlNode, Junctible {
+open class SimpleCondition: SqlNode, Junctible, SingleParameterizable, Expressible {
   var junction: Junction = .AND
   var expression = ""
+  lazy var param: SqlParameter? = SqlParameter()
+
   override open func toSql() -> String {
     return "\(prefixJunction())\(expression)"
   }
