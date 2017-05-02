6d50d6e6771b58e3827e676e9c178f236aeb1e5c
jerry <eeit54@gmail.com>
2017-05-02 09:58:37 +0800

    Modify SimpleConditionTests testcases

--- Tests/SqlNodeTests/SimpleConditionTests.swift
+++ Tests/SqlNodeTests/SimpleConditionTests.swift
@@ -5,10 +5,10 @@ class SimpleConditionTests: XCTestCase {
   func toSql() {
     let sc = SimpleCondition()
     sc.expression = "p.guid = 'uier009'"
-    XCTAssertEqual("AND p.guid = 'uier009'", sc.toSql())
+    XCTAssertEqual("p.guid = 'uier009'", sc.toSql())
 
     sc.junction = .OR
-    XCTAssertEqual("OR p.guid = 'uier009'", sc.toSql())
+    XCTAssertEqual("p.guid = 'uier009'", sc.toSql())
   }
   static var allTests = [
       ("toSql", toSql)
