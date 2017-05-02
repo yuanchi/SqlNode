05f0e34c98f8a6b18ec5b26691f21d2637e64ca9
jerry <eeit54@gmail.com>
2017-05-02 13:56:49 +0800

    Aliasible limited with class and remove mutating modifier

--- Sources/Aliasible.swift
+++ Sources/Aliasible.swift
@@ -1,17 +1,17 @@
-public protocol Aliasible {
+public protocol Aliasible: class {
   var alias: String { get set }
-  mutating func `as`(with alias: String)
+  func `as`(with alias: String)
 }
 public protocol SelfAliasible: Aliasible {
   associatedtype Me
-  mutating func `as`(_ alias: String) -> Me
+  func `as`(_ alias: String) -> Me
 }
 // providing default implementation to change self alias property(state)
 extension SelfAliasible {
-  mutating public func `as`(with alias: String) {
+  public func `as`(with alias: String) {
     self.alias = alias
   }
-  mutating public func `as`(_ alias: String) -> Me {
+  public func `as`(_ alias: String) -> Me {
     `as`(with: alias)
     return self as! Me
   }
