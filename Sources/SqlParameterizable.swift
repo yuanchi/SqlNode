79a691c5f0ff4e6dc0a45f8c51f3e752a97c450a
jerry <eeit54@gmail.com>
2017-05-03 11:10:15 +0800

    SqlParameterizable add sub protocol MultiParameterizable and its default implementation

--- Sources/SqlParameterizable.swift
+++ Sources/SqlParameterizable.swift
@@ -1,6 +1,6 @@
 protocol SqlParameterizable {}
 
-protocol SingleParameterizable: SqlParameterizable{
+protocol SingleParameterizable: SqlParameterizable {
   var param: SqlParameter? { get set }
   var paramVal: Any? { get set }
 }
@@ -15,5 +15,31 @@ extension SingleParameterizable {
         p.val = newValue
       }
     }
+  }
+}
+
+protocol MultiParameterizable: class, SqlParameterizable {
+  var params: [SqlParameter] { get set }
+  subscript(idx: Int) -> Any? { get set }
+  func addParamVal(_ val: Any) -> MultiParameterizable
+}
+
+extension MultiParameterizable {
+  subscript(idx: Int) -> Any? {
+    get {
+      guard idx < params.endIndex else {
+        return nil
+      }
+      return params[idx].val
+    }
+    set {
+      params[idx].val = newValue
+    }
+  }
+  func addParamVal(_ val: Any) -> MultiParameterizable {
+    let param = SqlParameter()
+    param.val = val
+    params.append(param)
+    return self
   }
 }
