2cde883c326224bb9dec67596137e80855fe9750
jerry <eeit54@gmail.com>
2017-05-03 15:28:19 +0800

    PageNavigator add stored property current

--- Sources/PageNavigator.swift
+++ Sources/PageNavigator.swift
@@ -2,37 +2,40 @@ public class PageNavigator {
     public var totalCount = 0
     public var previousPage = 1
     public var nextPage = 1
+    var current = 1
     public var currentPage: Int {
       get {
-        return self.currentPage
+        return self.current
       }
-      set(currentPage) {
-        var cp = currentPage
-        if currentPage <= 1 {
+      set {
+        var currentPage = newValue
+        if newValue <= 1 {
           previousPage = 1
-          cp = 1
+          currentPage = 1
         } else {
           previousPage = currentPage - 1
         }
-        if currentPage >= totalPageCount {
+        if newValue >= totalPageCount {
           nextPage = totalPageCount
-          cp = totalPageCount
+          currentPage = totalPageCount
         } else {
           nextPage = currentPage + 1
         }
-        self.currentPage = cp
+        self.current = currentPage
       }
     }
     public var totalPageCount = 1
     public var countPerPage = 10
+    public var offset: Int {
+      return countPerPage * (currentPage - 1)
+    }
 
     init() {}
-    
+
     init(totalCount: Int, countPerPage: Int) {
       self.totalCount = totalCount
       self.countPerPage = countPerPage
       countPageSize()
-      currentPage = 1
     }
 
     public func countPageSize() {
