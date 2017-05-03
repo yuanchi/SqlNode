f605e55dd13a5e6d48903435c90749fe3800ae55
jerry <eeit54@gmail.com>
2017-05-03 13:40:54 +0800

    RootNode add method paramVals()

--- Sources/RootNode.swift
+++ Sources/RootNode.swift
@@ -10,5 +10,25 @@ open class RootNode: SelectExpression {
     let copy = super.copy() as! RootNode
     return copy
   }
-  
+  /*
+  * collect all paramter values, conforming to SqlParameterizable
+  */
+  public func paramVals() -> [Any] {
+    var first = find { $0 is SingleParameterizable }
+      .flatMap { ($0 as? SingleParameterizable)?.paramVal }
+
+    let second = find { $0 is MultiParameterizable }
+      .flatMap {
+        ($0 as? MultiParameterizable)?.params.flatMap { $0.val }
+      }/*
+      .reduce([Any]()) { (results, vals) in
+        var merged = results
+        merged = merged + vals
+        return merged
+      }*/
+      .reduce([], +)
+
+    first = first + second
+    return first
+  }
 }
--- Tests/SqlNodeTests/RootNodeTests.swift
+++ Tests/SqlNodeTests/RootNodeTests.swift
@@ -9,9 +9,51 @@ class RootNodeTests: XCTestCase {
         .add(with: TestNode.self, { TestNode() })
         .replace(with: From.self, { From() })}
   }
+  func paramVals() {
+    let root = RootNode.initialize()
+      .select("e.id", "e.firstname", "e.lastname")
+      .from("EMPLOYEE", as: "e")
+      .where {
+        _ = $0.and("e.salary > ?").paramVal(3000)
+          .or {
+            _ = $0.and("e.startDate > ?").paramVal("2011-09-12")
+              .and("e.endDate < ?").paramVal("2015-10-11")
+          }
+      }
+      .orderBy("e.salary DESC")
 
+    let extra = ParameterizableExpression("LIMIT ? OFFSET ?")
+    _ = extra.addParamVal(10).addParamVal(1)
+    _ = root.add(child: extra)
+
+    let sql = "SELECT e.id, e.firstname, e.lastname\n"
+      + "FROM EMPLOYEE as e\n"
+      + "WHERE e.salary > ? OR (e.startDate > ? AND e.endDate < ?)\n"
+      + "ORDER BY e.salary DESC\n"
+      + "LIMIT ? OFFSET ?"
+    XCTAssertEqual(sql, root.toSql())
+
+    var paramVals = root.paramVals()
+    XCTAssertEqual(5, paramVals.count)
+    XCTAssertEqual(3000, paramVals[0] as! Int)
+    XCTAssertEqual("2011-09-12", paramVals[1] as! String)
+    XCTAssertEqual("2015-10-11", paramVals[2] as! String)
+    XCTAssertEqual(10, paramVals[3] as! Int)
+    XCTAssertEqual(1, paramVals[4] as! Int)
+
+    let f = root[.expression("e.endDate < ?")]
+    (f as! SimpleCondition).paramVal = nil
+
+    paramVals = root.paramVals()
+    XCTAssertEqual(4, paramVals.count)
+    XCTAssertEqual(3000, paramVals[0] as! Int)
+    XCTAssertEqual("2011-09-12", paramVals[1] as! String)
+    XCTAssertEqual(10, paramVals[2] as! Int)
+    XCTAssertEqual(1, paramVals[3] as! Int)
+  }
 
   static var allTests = [
       ("initialize", initialize),
+      ("paramVals", paramVals),
   ]
 }
