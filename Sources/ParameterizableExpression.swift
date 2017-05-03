9f8e17bc385d0298d9c7fc4fa427280d32eb489e
jerry <eeit54@gmail.com>
2017-05-03 13:40:02 +0800

    Debug

--- Sources/ParameterizableExpression.swift
+++ Sources/ParameterizableExpression.swift
@@ -1,7 +1,7 @@
 open class ParameterizableExpression: CustomExpression, MultiParameterizable {
   lazy public var params: [SqlParameter] = []
-  override open copy() -> ParameterizableExpression {
-    let copy = super.copy() as ! ParameterizableExpression
+  override open func copy() -> ParameterizableExpression {
+    let copy = super.copy() as! ParameterizableExpression
     copy.params = self.params
     return copy
   }
