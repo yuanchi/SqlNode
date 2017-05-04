bf9287b27594974f9b58ead6207702627c45667c
jerry <eeit54@gmail.com>
2017-05-04 15:21:58 +0800

    Add JoinSubquery

--- Sources/JoinSubquery.swift
+++ Sources/JoinSubquery.swift
@@ -10,4 +10,20 @@ open class JoinSubquery: JoinExpression {
     config(se)
     return self
   }
+  public func `as`(with alias: String) {
+    if let found = children.first(where: { $0 is SelectExpression }) as? SelectExpression {
+      found.alias = alias
+    }
+  }
+  override open func copy() -> JoinSubquery {
+    let copy = super.copy() as! JoinSubquery
+    return copy
+  }
+  override open func toSql() -> String {
+    let childrenSql = sqlChildren.map { $0.toSql() }.joined(separator: "\n ")
+    let r = [joinType, childrenSql]
+      .filter { !$0.isEmpty }
+      .joined(separator: " ")
+    return r
+  }
 }
