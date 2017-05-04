f683436f66633820a88430606c6680e77d83d815
jerry <eeit54@gmail.com>
2017-05-04 15:21:37 +0800

    Add JoinExpression

--- Sources/JoinExpression.swift
+++ Sources/JoinExpression.swift
@@ -1,7 +1,5 @@
-open class JoinExpression: SqlNode, SelfAliasible, Expressible  {
+open class JoinExpression: SimpleExpression  {
   public typealias Me = JoinExpression
-  public var alias: String = ""
-  public var expression: String = ""
   public var joinType: String = ""
 
   public func join(with type: String) -> Self {
@@ -13,11 +11,26 @@ open class JoinExpression: SqlNode, SelfAliasible, Expressible  {
     _ = add(child: on)
     return on
   }
-  public func on(_ conditions: String...) -> Self {
+  public func on(_ conditions: [String]) -> Self {
     let on = self.on()
     for c in conditions {
       _ = on.and(c)
     }
     return self
+  }
+  override open func toSql() -> String {
+    let aliasExpr = alias.isEmpty ? alias : "as \(alias)"
+    var r = [joinType, expression, aliasExpr]
+      .filter { !$0.isEmpty }
+      .joined(separator: " ")
+
+    let childrenSql = sqlChildren.map { $0.toSql() }.joined(separator: " ")
+    r = childrenSql.isEmpty ? r : (r + "\n " + childrenSql)
+    return r
+  }
+  override open func copy() -> JoinExpression {
+    let copy = super.copy() as! JoinExpression
+    copy.joinType = self.joinType
+    return copy
   }
 }
