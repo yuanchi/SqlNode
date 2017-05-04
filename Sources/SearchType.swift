63a21052a4be7ba5650ca0366e3102bf9b3df3c7
jerry <eeit54@gmail.com>
2017-05-04 09:49:12 +0800

    Modify SearchType conforming to SingleNodeSearchable and implement get(from:) method

--- Sources/SearchType.swift
+++ Sources/SearchType.swift
@@ -1,16 +1,33 @@
-public enum SearchType {
+public enum SearchType: SingleNodeSearchable {
   case expression(String)
   case id(String)
   case alias(String)
 
-  public var val: String {
+  public func get(from start: SqlNode) -> SqlNode? {
+    var found: SqlNode? = nil
     switch self {
-    case .id(let v):
-        return v
-    case .alias(let v):
-        return v
-    case .expression(let v):
-        return v
+    case .id(let val):
+      found = start.findFirst { n in
+        if let casted = n as? Identifiable {
+          return casted.id == val
+        }
+        return false
+      } as? SqlNode
+    case .alias(let val):
+      found = start.findFirst { n in
+        if let casted = n as? Aliasible {
+          return casted.alias == val
+        }
+        return false
+      } as? SqlNode
+    case .expression(let val):
+      found = start.findFirst { n in
+        if let casted = n as? Expressible {
+          return casted.expression == val
+        }
+        return false
+      } as? SqlNode
     }
+    return found
   }
 }
